00f960e3b921320ba347b6e7b9df98e29b340b6c
Francisco Cobas <fcarocena@gmail.com>
2022-01-18 19:28:07 -0300

    Add proveedores hosting page

---
+++ app/routes/__app/proveedores-hosting.tsx
@@ -0,0 +1,45 @@
+import { HostingProvider } from "@prisma/client";
+import { LoaderFunction, useLoaderData } from "remix";
+
+import { db } from "~/utils/db.server";
+
+type LoaderData = {
+  hostingProviders: HostingProvider[];
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const hostingProviders = await db.hostingProvider.findMany({
+    orderBy: { name: "asc" },
+  });
+
+  const data: LoaderData = { hostingProviders };
+
+  return data;
+};
+
+export default function HostingProvidersRoute() {
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <section className="max-w-screen-xl mx-auto">
+      <table>
+        <thead>
+          <tr>
+            <th className="border border-gray-400 p-2">Proveedor</th>
+            <th className="border border-gray-400 p-2">Nombre del Servidor</th>
+            <th className="border border-gray-400 p-2">Vencimiento</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.hostingProviders.map((provider) => (
+            <tr key={provider.id}>
+              <td className="border border-gray-400 p-2">{provider.provider}</td>
+              <td className="border border-gray-400 p-2">{provider.name}</td>
+              <td className="border border-gray-400 p-2">{provider.expirationDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+}
